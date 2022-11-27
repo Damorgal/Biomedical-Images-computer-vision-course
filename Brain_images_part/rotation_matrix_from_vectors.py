01415c88caa5313544496433320c161d1b5ff1c9
Diego Moreno <72321734+Damorgal@users.noreply.github.com>
2022-11-27 17:17:05 -0600

    Project submission

---
+++ Brain_images_part/rotation_matrix_from_vectors.py
@@ -0,0 +1,46 @@
+import numpy as np
+import math
+
+def rotation_matrix_from_vectors(vec1, vec2):
+    """ Find the rotation matrix that aligns vec1 to vec2
+    :param vec1: A 3d "source" vector
+    :param vec2: A 3d "destination" vector
+    :return mat: A transform matrix (3x3) which when applied to vec1, aligns it with vec2.
+    """
+    a, b = (vec1 / np.linalg.norm(vec1)).reshape(3), (vec2 / np.linalg.norm(vec2)).reshape(3)
+    v = np.cross(a, b)
+    c = np.dot(a, b)
+    s = np.linalg.norm(v)
+    kmat = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
+    rotation_matrix = np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2))
+    return rotation_matrix
+
+def random_three_vector():
+    """
+    Generates a random 3D unit vector (direction) with a uniform spherical distribution
+    Algo from http://stackoverflow.com/questions/5408276/python-uniform-spherical-distribution
+    :return:
+    """
+    phi = np.random.uniform(0,np.pi*2)
+    costheta = np.random.uniform(-1,1)
+
+    theta = np.arccos( costheta )
+    x = np.sin( theta) * np.cos( phi )
+    y = np.sin( theta) * np.sin( phi )
+    z = np.cos( theta )
+    return (x,y,z)
+
+vec1 = [1, 0, 0]
+
+
+vec2 = random_three_vector()
+
+
+print(vec2)
+
+mat = rotation_matrix_from_vectors(vec1, vec2)
+
+vec1_rot = mat.dot(vec1)
+
+print(vec1_rot)
+
---
+++ Retina_angiograms_STENOSIS_images_part/Stenosis_PROJECT/README.md
@@ -0,0 +1,10 @@
+# Stenosis Detection Final Project of Computer Vision (Biomedical Images) Course
+
+The final project of the graduate course in computer vision with biomedical images.
+ This project is a deep learning approach for biomedical purposes of stenosis detection. We fine-tuned a YOLO NN using the Roboflow tool with images of stenosis and non-stenosis patches.
+ 
+ ## Example of the detection
+ 
+ ![ex1](Final/e1)
+ 
+  ![ex1](Final/e2)
